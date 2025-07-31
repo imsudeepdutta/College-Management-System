65e8d32779db7946798e57294ccd142e206cd377
imsudeepdutta@gmail.com <imsudeepdutta@gmail.com>
2025-08-01 01:03:08 +0530

    Add root route to fix Cannot GET /

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => 
+{
+  res.send('College Management System Backend is running');
+});
+
 // MySQL connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
